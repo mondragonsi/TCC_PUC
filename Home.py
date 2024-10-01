9b6b93a63e95efa363c935982534bd3f6e033af7
Bruno Vieira Mondragon <mondragon.si@gmail.com>
2024-09-30 22:53:46 -0300

    Add chain chat functionality to Home.py

--- Home.py
+++ Home.py
@@ -1,8 +1,14 @@
 import streamlit as st
 from pathlib import Path
+import time
 
 
 FILES_FOLDER = Path(__file__).parent / "files"
+
+def create_chain_chat():
+    st.session_state.chain = True
+    time.sleep(1)
+    pass
 
 def sidebar():
     """ Sidebar for navigation Everything ran here will be executed in the sidebar"""
@@ -18,7 +24,17 @@ def sidebar():
         for pdf in uploaded_pdfs:
             with open(FILES_FOLDER / pdf.name, "wb") as file:
                 file.write(pdf.read())
-    
+                
+    label_button = 'Iniciar ChatBot'
+    if 'chain' in st.session_state:
+        label_button = 'Atualizar ChatBot'
+    if st.button(label_button, use_container_width=True):
+        if len(list(FILES_FOLDER.glob("*.pdf"))) == 0:
+            st.error("Nenhum arquivo PDF foi carregado. Adicione um arquivo PDF iniciar o chatbot.")
+        else:
+            st.success("Iniciando chatbot...")
+            create_chain_chat()
+            st.rerun()
 
 def main():
    with st.sidebar:
