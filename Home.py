beec79452d71ab0912aaaef81020af08e8796b4d
Bruno Vieira Mondragon <mondragon.si@gmail.com>
2024-10-01 18:18:20 -0300

    Add chat window and conversation memory

--- Home.py
+++ Home.py
@@ -1,18 +1,26 @@
 import streamlit as st
 from pathlib import Path
 import time
+from langchain.memory import ConversationBufferMemory
 
 
 FILES_FOLDER = Path(__file__).parent / "files"
 
 def create_chain_chat():
     st.session_state.chain = True
+    
+    
+    memory = ConversationBufferMemory(return_messages=True)
+    memory.chat_memory.add_user_message('Olá! Sou Usuario! :)')
+    memory.chat_memory.add_ai_message('Olá! Como posso te ajudar? Sou uma LLM :)')
+    st.session_state.memory = memory
+    
     time.sleep(1)
     pass
 
 def sidebar():
     """ Sidebar for navigation Everything ran here will be executed in the sidebar"""
-    st.sidebar.title("Navigation")
+    st.sidebar.title("Proposições Políticas em PDF")
     uploaded_pdfs = st.file_uploader("Adicione as preoposições em PDF aqui",
                      type=".pdf", 
                      key="pdf_file",
@@ -36,9 +44,44 @@ def sidebar():
             create_chain_chat()
             st.rerun()
 
+def chat_window():
+    st.header("🇧🇷 ChatBot para Proposições Políticas", divider=True)
+    
+    if 'chain' not in st.session_state:
+        st.error("Nenhum chatbot foi iniciado. Adicione um arquivo PDF para iniciar o chatbot.")
+        st.stop() # Stop the script here
+        
+    # chain = st.session_state.chain
+    # memory = st.session_state.memory
+    
+    memory = st.session_state.memory
+    messages = memory.load_memory_variables({})['history']
+    
+    container = st.container()
+    for message in messages:
+        chat = container.chat_message(message.type)
+        chat.markdown(message.content)
+        
+    new_message = st.chat_input("Escreva sua pergunta sobre a preposicao")
+    if new_message is not None:
+        chat = container.chat_message("human")
+        chat.markdown(new_message)
+        
+        chat = container.chat_message("ai")
+        chat.markdown('gerando resposta...')
+        
+        time.sleep(2)
+        memory.chat_memory.add_user_message(new_message)
+        memory.chat_memory.add_ai_message('OI SOU O CHATBOT LLM aqui de novo!')
+        st.rerun()
+        
+        
+    
+
 def main():
    with st.sidebar:
-         sidebar()
+        sidebar()
+   chat_window()
     
 if __name__ == "__main__":
     main()
